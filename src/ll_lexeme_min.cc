fcdb47bd2e353c52d773292c1d4887effb395d9a
walton <walton>
2010-04-06 15:46:13 +0000

    update

---
+++ src/ll_lexeme_min.cc
@@ -0,0 +1,26 @@
+// Layered Language Lexical Analyzer MIN Interface
+//
+// File:	ll_lexeme_min.cc
+// Author:	Bob Walton (walton@deas.harvard.edu)
+// Date:	Fri Apr  2 18:12:57 EDT 2010
+//
+// The authors have placed this program in the public
+// domain; they make no warranty and accept no liability
+// for this program.
+//
+// RCS Info (may not be true date or author):
+//
+//   $Author: walton $
+//   $Date: 2010/04/06 15:46:13 $
+//   $RCSfile: ll_lexeme_min.cc,v $
+//   $Revision: 1.1 $
+
+# include <ll_lexeme>
+
+# define LLLEX ll::lexeme
+
+min::uns32 ** create_program ( min::uns32 size )
+{
+}
+
+void resize_program ( min::uns32 new_size );

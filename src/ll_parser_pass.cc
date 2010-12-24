d44f49444f4aaffdb111336f084720ec86b3df62
Robert L. Walton <walton@acm.org>
2010-12-24 10:18:38 -0500

    update

--- src/ll_parser_pass.cc
+++ src/ll_parser_pass.cc
@@ -2,7 +2,7 @@
 //
 // File:	ll__parser_pass.cc
 // Author:	Bob Walton (walton@acm.org)
-// Date:	Tue Dec 21 07:45:19 EST 2010
+// Date:	Fri Dec 24 09:10:37 EST 2010
 //
 // The authors have placed this program in the public
 // domain; they make no warranty and accept no liability
@@ -54,19 +54,50 @@ static min::packed_struct<PAR::token_struct>
 
 // Free list of tokens.
 //
-min::static_stub<1> token_vec;
+static min::static_stub<1> token_vec;
 PAR::token_ptr & free_tokens =
     * ( PAR::token_ptr *) & token_vec[0];
+
+static int number_free_tokens = 0;
+
+static int max_token_free_list_size = 1000;
 
 PAR::token_ptr PAR::new_token ( min::uns32 kind )
 {
     token_ptr token = remove ( ::free_tokens );
     if ( token == min::NULL_STUB )
         token = ::token_type.new_stub();
+    else
+        -- ::number_free_tokens;
     token->value = min::MISSING;
     token->string = min::NULL_STUB;
     token->kind = kind;
     return token;
+}
+
+void PAR::free_token ( token_ptr token )
+{
+    if ( ::max_token_free_list_size >= 0
+         &&
+            ::number_free_tokens
+	 >= ::max_token_free_list_size )
+    {
+        min::deallocate ( token );
+	return;
+    }
+
+    put_at_end ( ::free_tokens, token );
+    ++ ::number_free_tokens;
+}
+
+void PAR::set_max_token_free_list_size ( int n )
+{
+    ::max_token_free_list_size = n;
+    if ( n >= 0 ) while ( ::number_free_tokens > n )
+    {
+        min::deallocate ( remove ( ::free_tokens ) );
+	-- ::number_free_tokens;
+    }
 }
 
 // Passes

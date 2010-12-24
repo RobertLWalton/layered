85686fe92d3cb036d2150831e3460d2ed7b8e99b
Robert L. Walton <walton@acm.org>
2010-12-24 17:40:45 -0500

    update

--- src/ll_parser_pass.cc
+++ src/ll_parser_pass.cc
@@ -2,7 +2,7 @@
 //
 // File:	ll__parser_pass.cc
 // Author:	Bob Walton (walton@acm.org)
-// Date:	Fri Dec 24 09:10:37 EST 2010
+// Date:	Fri Dec 24 12:59:43 EST 2010
 //
 // The authors have placed this program in the public
 // domain; they make no warranty and accept no liability
@@ -29,6 +29,83 @@
 
 // Strings
 // -------
+
+static min::uns32 string_stub_disp[] =
+{
+    min::DISP ( & PAR::token_struct::next ),
+    min::DISP_END
+};
+
+static min::packed_vec<PAR::string_struct,min::uns32>
+    string_type ( "ll::parser::string",
+                  NULL, ::string_stub_disp );
+
+// Free list of strings.
+//
+static min::static_stub<1> string_vec;
+PAR::string_insptr & free_strings =
+    * ( PAR::string_insptr *) & string_vec[0];
+
+static int number_free_strings = 0;
+
+static int max_string_free_list_size = 100;
+
+static int min_string_length = 80;
+
+PAR::string_ptr PAR::new_string
+	( min::uns32 n, min::uns32 * string )
+{
+    string_insptr str = ::free_strings;
+    if ( str == min::NULL_STUB )
+    {
+        min::uns32 m = n;
+	if ( m < ::min_string_length )
+	    m = ::min_string_length;
+        str = ::string_type.new_stub ( m );
+    }
+    else
+    {
+        -- ::number_free_strings;
+	::free_strings = str->next;
+	if ( str->length < n )
+	    resize ( str, n );
+	pop ( str, str->length,
+	      (min::uns32 *) NULL );
+    }
+    str->next = min::NULL_STUB;
+    push ( str, n, string );
+    return (string_ptr) str;
+}
+
+PAR::string_ptr PAR::free_string ( string_ptr string )
+{
+    if ( ::max_string_free_list_size >= 0
+         &&
+            ::number_free_strings
+	 >= ::max_string_free_list_size )
+    {
+        min::deallocate ( string );
+	return min::NULL_STUB;
+    }
+
+    string_insptr str = (string_insptr) string;
+    str->next = ::free_strings;
+    ::free_strings = str;
+    ++ ::number_free_strings;
+    return min::NULL_STUB;
+}
+
+void PAR::set_max_string_free_list_size ( int n )
+{
+    ::max_string_free_list_size = n;
+    if ( n >= 0 ) while ( ::number_free_strings > n )
+    {
+	string_insptr string = ::free_strings;
+	::free_strings = string->next;
+        min::deallocate ( string );
+	-- ::number_free_strings;
+    }
+}
 
 // Tokens
 // ------
@@ -86,6 +163,8 @@ void PAR::free_token ( token_ptr token )
 	return;
     }
 
+    token->value = min::MISSING;
+    token->string = free_string ( token->string );
     put_at_end ( ::free_tokens, token );
     ++ ::number_free_tokens;
 }

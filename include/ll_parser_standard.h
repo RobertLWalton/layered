854da1c9acf868d7bf88d78f8df316850d983cd0
Robert L. Walton <walton@acm.org>
2010-12-27 23:03:38 -0500

    update

--- include/ll_parser_standard.h
+++ include/ll_parser_standard.h
@@ -2,7 +2,7 @@
 //
 // File:	ll_parser_standard.h
 // Author:	Bob Walton (walton@acm.org)
-// Date:	Mon Dec 13 19:08:32 EST 2010
+// Date:	Mon Dec 27 21:44:07 EST 2010
 //
 // The authors have placed this program in the public
 // domain; they make no warranty and accept no liability
@@ -32,47 +32,49 @@ namespace ll { namespace parser { namespace standard {
     // The lexeme types as in ll_lexeme_standard.h are
     // processed as follows:
     //
-    //    word		    NAME, SYMBOL, string value
-    //    natural_number    if < 2**28:
-    //				NAME, NATURAL_NUMBER,
-    //				      number value
+    //    word_t	    SYMBOL, string value
+    //    natural_number_t  if < 2**28:
+    //				NATURAL_NUMBER,
+    //				number value
     //			    else:
-    //			        LEXEME, natural_number,
-    //				        translated
-    //					string
-    //    number	    LEXEME, number,
-    //				    translated string
-    //    mark		    NAME, SYMBOL, string value
-    //    quoted_string	    LEXEME, quoted_string,
-    //				    translated string
-    //    separator	    NAME, SYMBOL, string value
-    //    comment	    no token
-    //    horizontal_space  no token
-    //    line_break        LEXEME, line_break,
-    //				    translated string
-    //    end_of_file       LEXEME, end_of_file,
-    //				    translated string
-    //    bad_end_of_line   LEXEME, line_break,
-    //				    translated string,
-    //				    output error message
-    //    bad_end_of_file   no token,
-    //				    output error message
-    //    unrecognized_character
+    //			        natural_number_t,
+    //				string
+    //    number_t	    number_t, string
+    //    mark_t	    SYMBOL, string value
+    //    quoted_string_t   quoted_string_t, string
+    //    separator_t	    SYMBOL, string value
+    //    comment_t	    no token
+    //    horizontal_space_t
+    //			    no token
+    //    line_break_t      line_break_t, string
+    //    end_of_file_t     end_of_file_t, string
+    //    bad_end_of_line_t line_break_t, string,
+    //			    output error message
+    //    bad_end_of_file_t no token,
+    //			    output error message
+    //    unrecognized_character_t
     //			    no token,
-    //				    output error message
-    //    unrecognized_escape_character
+    //			    output error message
+    //    unrecognized_escape_character_t
     //			    no token,
-    //				    output error message
+    //			        output error message
     //			    erroneous atom,
-    //				    output error message
-    //    unrecognized_escape_sequence
+    //			        output error message
+    //    unrecognized_escape_sequence_t
     //			    no token,
-    //				    output error message
+    //			        output error message
     //			    erroneous atom,
-    //				    output error message
-    //    non_letter_escape_sequence
+    //			        output error message
+    //    non_letter_escape_sequence_t
     //			    erroneous atom,
-    //				    output error message
+    //				output error message
+    //
+    // The token kind is either the lexeme type or
+    // SYMBOL or NATURAL_NUMBER.
+    //
+    // The translation_buffer is recorded as either
+    // a min::gen `string value' for SYMBOLs or a
+    // ll::parser::string for lexeme type kinds.
     //
     pass_ptr create_input_pass
         ( ll::lexeme::scanner_ptr scanner );

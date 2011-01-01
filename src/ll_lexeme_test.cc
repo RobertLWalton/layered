79748da304861bbe78b2efc760ff44bf657c8717
Robert L. Walton <walton@acm.org>
2011-01-01 03:19:07 -0500

    update

--- src/ll_lexeme_test.cc
+++ src/ll_lexeme_test.cc
@@ -2,7 +2,7 @@
 //
 // File:	ll_lexeme_test.cc
 // Author:	Bob Walton (walton@acm.org)
-// Date:	Thu Dec 30 07:46:43 EST 2010
+// Date:	Sat Jan  1 03:18:27 EST 2011
 //
 // The authors have placed this program in the public
 // domain; they make no warranty and accept no liability
@@ -81,8 +81,6 @@ void LEX::basic_test_input
 // Input Test
 // ----- ----
 
-
-
 min::static_stub<2> input_vec;
 static min::packed_vec_insptr<char>
     & lexeme_codes =
@@ -91,10 +89,107 @@ static min::packed_vec_insptr<char>
     & erroneous_atom_codes =
         * (min::packed_vec_insptr<char> *) input_vec[1];
 
+static min::packed_vec<char> codes_type
+    ( "ll::lexeme::codes_type" );
+
+static const char * type_code;
+    // Save of type_code argument for general use.
+
+static LEX::uns32 next_line;
+    // Line number of next line to be printed.
+    // 0 if no lines printed yet.
+static LEX::uns32 line_width;
+    // Line width of last line printed.
+
+// Set next_line = line + 1.
+//
+static void set_line ( uns32 line )
+{
+    assert ( ::next_line <= line );
+    if ( ::next_line == 0 )
+    {
+	if ( lexeme_codes == min::NULL_STUB )
+	{
+	    lexeme_codes = codes_type.new_stub();
+	    erroneous_atom_codes =
+	        codes_type.new_stub();
+	}
+    }
+    else
+    {
+        for ( uns32 i = 0;
+	      i < erroneous_atom_codes->length; ++ i )
+	{
+	    char c = erroneous_atom_codes[i];
+	    if ( c == ' ' ) continue;
+	    while ( i >= lexeme_codes->length )
+	        min::push(lexeme_codes) = ' ';
+	    lexeme_codes[i] = c;
+	}
+	min::pop ( erroneous_atom_codes,
+	           erroneous_atom_codes->length );
+	if ( lexeme_codes->length > 0 )
+	{
+	    min::push(lexeme_codes) = 0;
+	    cout << & lexeme_codes[0] << endl;
+	    min::pop ( lexeme_codes,
+	               lexeme_codes->length );
+	}
+    }
+
+    LEX::scanner_ptr scanner = LEX::default_scanner;
+    while ( ::next_line <= line )
+    {
+        uns32 offset =
+	    LEX::next_line ( scanner->input_file );
+	const char * p =
+	    & scanner->input_file->data[offset];
+	char buffer [10 + 4 * strlen ( p )];
+	::line_width = 0;
+	uns32 boffset =
+	    LEX::spstring ( buffer, p,
+	                    ::line_width, 0, scanner,
+			    LEX::UTF8GRAPHIC );
+	LEX::spstring ( buffer + boffset, "\n",
+	                ::line_width, 0, scanner,
+			LEX::UTF8GRAPHIC );
+	++ ::next_line;
+    }
+
+    assert ( next_line == line + 1 );
+}
+
+static void set_codes
+	( min::packed_vec_insptr<char> codes,
+	  uns32 first, uns32 last, uns32 type )
+{
+    LEX::scanner_ptr scanner = LEX::default_scanner;
+
+    uns32 begin_column =
+        scanner->input_buffer[first].column;
+    uns32 end_column =
+        last + 1 < scanner->input_buffer->length ?
+        scanner->input_buffer[last+1].column :
+	scanner->next_position.column;
+    uns32 line = scanner->input_buffer[first].line;
+    if ( line <= ::next_line ) set_line ( line );
+
+    if ( end_column <= begin_column )
+        end_column = ::line_width;
+
+    while ( codes->length < end_column )
+        min::push(codes) = ' ';
+    for ( uns32 i = begin_column; i < end_column; ++ i )
+        codes[i] = ::type_code[type];
+}
+
 static void erroneous_atom
 	( uns32 first, uns32 last, uns32 type,
 	  LEX::scanner_ptr scanner )
 {
+    assert ( scanner == LEX::default_scanner );
+    set_codes ( ::erroneous_atom_codes,
+                first, last, type );
 }
 
 void LEX::test_input
@@ -103,10 +198,13 @@ void LEX::test_input
 	  const char * file_name,
 	  uns32 end_of_file_t )
 {
+    ::type_code = type_code;
+
     init_scanner();
-    default_scanner->erroneous_atom =
-        ::erroneous_atom;
-    init_stream ( default_scanner->input_file,
+    scanner_ptr scanner = LEX::default_scanner;
+
+    scanner->erroneous_atom = ::erroneous_atom;
+    init_stream ( scanner->input_file,
                   in, file_name, 0 );
 
     while ( true )
@@ -116,13 +214,14 @@ void LEX::test_input
 	if ( type == LEX::SCAN_ERROR )
 	{
 	    cout << "Scan Error" << endl
-	         << LEX::default_scanner->error_message
+	         << scanner->error_message
 		 << endl;
 	    return;
 	}
 	else
-	{
-	}
+	    set_codes ( lexeme_codes,
+	                first, last, type );
+
 	if ( type == end_of_file_t ) break;
     }
 }

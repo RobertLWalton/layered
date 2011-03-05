6c1d81d8e6267933f42469982288a63ee5399b88
Robert L. Walton <walton@acm.org>
2011-03-05 10:28:14 -0500

    update

--- include/ll_lexeme_standard.h
+++ include/ll_lexeme_standard.h
@@ -2,7 +2,7 @@
 //
 // File:	ll_lexeme_standard.h
 // Author:	Bob Walton (walton@acm.org)
-// Date:	Fri Feb 25 03:16:54 EST 2011
+// Date:	Sat Mar  5 04:38:31 EST 2011
 //
 // The authors have placed this program in the public
 // domain; they make no warranty and accept no liability
@@ -48,16 +48,11 @@ namespace ll { namespace lexeme { namespace standard {
     const uns32 bad_end_of_file_t		= 12;
     const uns32 unrecognized_character_t	= 13;
 
-    // Lexeme or Erroneous Atom Types
-
-    const uns32 unrecognized_escape_character_t	= 14;
-    const uns32 unrecognized_escape_sequence_t	= 15;
-
     // Erroneous Atom Types
     //
-    const uns32 non_letter_escape_sequence_t	= 16;
+    const uns32 non_letter_escape_sequence_t	= 14;
 
-    const unsigned MAX_TYPE = 16;
+    const unsigned MAX_TYPE = 14;
 
     extern const char * const type_name[MAX_TYPE+1];
 

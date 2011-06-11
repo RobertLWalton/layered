e155d258f7fc6ca899af6257c17c2a661fecca0c
Robert L. Walton <walton@acm.org>
2011-06-11 09:25:37 -0400

    update

--- include/ll_lexeme_c++.h
+++ include/ll_lexeme_c++.h
@@ -2,7 +2,7 @@
 //
 // File:	ll_lexeme_c++.h
 // Author:	Bob Walton (walton@acm.org)
-// Date:	Thu Jun  2 07:27:47 EDT 2011
+// Date:	Sat Jun 11 08:53:09 EDT 2011
 //
 // The authors have placed this program in the public
 // domain; they make no warranty and accept no liability
@@ -32,37 +32,77 @@ namespace ll { namespace lexeme { namespace cpp {
 
     // Lexeme Types
 
-    const uns32 horizontal_space_t		= 1;
+    const uns32 whitespace_t			= 1;
+        // Sequences of whitespace characters other than
+	// newline that are outside character and string
+	// literals.
     const uns32 comment_t			= 2;
+        // Either a /* */ or // comment.
     const uns32 bracketed_header_name_t		= 3;
     const uns32 quoted_header_name_t		= 4;
     const uns32 operator_t			= 5;
+        // Includes separators.
     const uns32 identifier_t			= 6;
     const uns32 dec_integer_t			= 7;
     const uns32 oct_integer_t			= 8;
     const uns32 hex_integer_t			= 9;
     const uns32 float_t				= 10;
     const uns32 pp_number_t			= 11;
-    const uns32 character_literal_t		= 12;
-    const uns32 u_character_literal_t		= 13;
-    const uns32 U_character_literal_t		= 14;
-    const uns32 L_character_literal_t		= 15;
+        // A pp number that is not an integer or float.
+    const uns32 char_literal_t			= 12;
+    const uns32 u_char_literal_t		= 13;
+    const uns32 U_char_literal_t		= 14;
+    const uns32 L_char_literal_t		= 15;
     const uns32 string_literal_t		= 16;
     const uns32 u_string_literal_t		= 17;
     const uns32 U_string_literal_t		= 18;
     const uns32 L_string_literal_t		= 19;
     const uns32 newline_t			= 20;
     const uns32 end_of_file_t			= 21;
+        // The parser should treat an end-of-file that
+	// is not preceeded by a newline as if it
+	// was proceeded by a newline.
 
     // Erroneous Atom Types
     //
     const uns32 premature_newline_t		= 22;
+        // 0-length, emitted before newline that
+	// terminates a character or string literal.
+	// A ' or " is inserted at the end of the
+	// literal.
     const uns32 premature_end_of_file_t		= 23;
-    const uns32 misplaced_character_t		= 24;
-    const uns32 ascii_universal_character_t	= 25;
-    const uns32 misplaced_vertical_space_t	= 26;
+        // 0-length, emitted before an end-of-file that
+	// terminates a character or string literal.
+	// A ' or " is inserted at the end of the
+	// literal.
+    const uns32 illegal_escape_t		= 24;
+        // The sequence \C inside a character or string
+	// literal where C represents a character such
+	// that \C does not begin a legal escape
+	// sequence.  Both \ and C are part of the
+	// erroneous atom.  The erroneous atom is
+	// translated to "" and ignored.
+    const uns32 misplaced_char_t		= 25;
+        // Backslash or a non-ascii, non-letter charac-
+	// ter outside a character or string literal.
+	// Note that ## cannot be used to move charac-
+	// ters inside a character or string literal,
+	// so once misplaced, always misplaced.  Also,
+	// we do NOT allow ## to append a combining
+	// character to another character.  These
+	// erroneous atoms are translated as "" and
+	// ignored.
+    const uns32 ascii_universal_char_t		= 26;
+        // A universal character that represents an
+	// ASCII character outside a character or
+	// string literal.  These erroneous atoms are
+	// translated as "" and ignored.
+    const uns32 ill_formed_universal_char_t	= 27;
+        // A universal character with non-hexadecimal
+	// digits.  These erroneous atoms are translated
+	// as "" and ignored.
 
-    const unsigned MAX_TYPE = 26;
+    const unsigned MAX_TYPE = 27;
 
     extern const char * const type_name[MAX_TYPE+1];
 

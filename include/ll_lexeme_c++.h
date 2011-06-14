a6dd61b76187726993624d895e5115a633a26be9
Robert L. Walton <walton@acm.org>
2011-06-14 05:08:02 -0400

    update

--- include/ll_lexeme_c++.h
+++ include/ll_lexeme_c++.h
@@ -2,7 +2,7 @@
 //
 // File:	ll_lexeme_c++.h
 // Author:	Bob Walton (walton@acm.org)
-// Date:	Sun Jun 12 10:26:57 EDT 2011
+// Date:	Tue Jun 14 04:00:55 EDT 2011
 //
 // The authors have placed this program in the public
 // domain; they make no warranty and accept no liability
@@ -44,11 +44,15 @@ namespace ll { namespace lexeme { namespace cpp {
         // Includes separators.
     const uns32 identifier_t			= 6;
     const uns32 decimal_integer_t		= 7;
+        // A pp number with decimal integer form.
     const uns32 octal_integer_t			= 8;
+        // A pp number with octal integer form.
     const uns32 hexadecimal_integer_t		= 9;
+        // A pp number with hexadecimal integer form.
     const uns32 float_t				= 10;
+        // A pp number with floating point form.
     const uns32 pp_number_t			= 11;
-        // A pp number that is not an integer or float.
+        // Other pp numbers.
     const uns32 char_literal_t			= 12;
     const uns32 u_char_literal_t		= 13;
     const uns32 U_char_literal_t		= 14;
@@ -58,6 +62,7 @@ namespace ll { namespace lexeme { namespace cpp {
     const uns32 U_string_literal_t		= 18;
     const uns32 L_string_literal_t		= 19;
     const uns32 newline_t			= 20;
+        // A single newline charater by itself.
     const uns32 end_of_file_t			= 21;
         // The parser should treat an end-of-file that
 	// is not preceeded by a newline as if it
@@ -75,8 +80,11 @@ namespace ll { namespace lexeme { namespace cpp {
     // inside a character or string literal '' or "".
     // Also, we do NOT allow ## to append a combining
     // character to another character.  So ## cannot be
-    // used to make these lexemes part of a legal
-    // lexeme.
+    // used to create a legal pp token by concatenating
+    // a legal pp token with a stray character that is
+    // not part of a legal pp token.  Therefore we can
+    // treat all stay characters as errors, instead of
+    // making them into pp tokens.
     //
     const uns32 misplaced_char_t		= 22;
         // Backslash or a non-ascii, non-letter charac-
@@ -115,8 +123,9 @@ namespace ll { namespace lexeme { namespace cpp {
     const uns32 too_long_hex_escape_t		= 29;
         // A \x or \X escape sequence with 9 hexadecimal
 	// digits.  This would be allowed in standard
-	// C++, but do to the limits of our lexeme
-	// translator, we do not permit such.
+	// C++, but due to the limitations of our lexeme
+	// translator, we must bound the length of any
+	// escape sequence.
     // const uns32 short_universal_char_t	= 25;
         // A universal character with to few digits.
 	// Has the same type code as error lexeme above.

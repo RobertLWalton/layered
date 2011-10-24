bac822f869470db81223c30952293038d4d63b65
Robert L. Walton <walton@acm.org>
2011-10-24 14:49:17 -0400

    update

---
+++ test/ll_lexeme_c++_test.cc
@@ -0,0 +1,43 @@
+// Layers Language C++ Lexical Analyzer Test
+//
+// File:	ll_lexeme_c++_test.cc
+// Author:	Bob Walton (walton@acm.org)
+// Date:	Sun Jun 19 03:03:43 EDT 2011
+//
+// The authors have placed this program in the public
+// domain; they make no warranty and accept no liability
+// for this program.
+
+// Table of Contents
+//
+//	Usage and Setup
+//	External Runtime
+//	Program Construction Test
+
+// Usage and Setup
+// ----- --- -----
+
+# include <ll_lexeme.h>
+# include <ll_lexeme_test.h>
+# include <ll_lexeme_c++.h>
+# include <iostream>
+# include <cassert>
+# define LEX ll::lexeme
+# define LEXCPP ll::lexeme::cpp
+
+int main ( int argc )
+{
+    LEX::init_output_stream
+	    ( LEX::default_scanner, std::cout )
+        << min::ascii;
+    LEXCPP::init_cpp_program();
+    LEX::init_program ( LEX::default_scanner,
+                        LEXCPP::default_program );
+    LEX::init_input_stream
+        ( LEX::default_scanner,
+	  std::cin,
+	    min::GRAPHIC_FLAGS
+	  + min::DISPLAY_EOL_FLAG );
+    LEX::test_input ( LEXCPP::type_code,
+                      LEXCPP::end_of_file_t );
+}

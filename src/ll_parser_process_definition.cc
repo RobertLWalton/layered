f06fe41649e02a57eebb6b9b248da2248e49efac
Robert L. Walton <walton@acm.org>
2011-10-25 05:37:17 -0400

    update

--- src/ll_parser_process_definition.cc
+++ src/ll_parser_process_definition.cc
@@ -2,7 +2,7 @@
 //
 // File:	ll_parser_process_definition.cc
 // Author:	Bob Walton (walton@acm.org)
-// Date:	Mon Oct 24 07:04:25 EDT 2011
+// Date:	Tue Oct 25 05:36:50 EDT 2011
 //
 // The authors have placed this program in the public
 // domain; they make no warranty and accept no liability
@@ -16,9 +16,19 @@
 // Usage and Setup
 // ----- --- -----
 
+# include <ll_parser.h>
 # include <ll_parser_table.h>
 # define PAR ll::parser
 # define TAB ll::parser::table
+
+min::locatable_gen DEFINE;
+
+struct initializer {
+    initializer ( void )
+    {
+        ::DEFINE = min::new_str_gen ( "define" );
+    }
+} init;
 
 // Parser Process Definition Function
 // ------ ------- ---------- --------
@@ -29,4 +39,10 @@ bool PAR::parser_process_definition
 {
     if ( expression->type != PAR::EXPRESSION )
         return false;
+    min::gen expr = expression->value;
+    if ( ! min::is_obj ( expr ) ) return false;
+    min::obj_vec_ptr vp ( expr );
+    min::unsptr length = size_of ( vp );
+    if ( length < 2 ) return false;
+    if ( vp[0] != DEFINE ) return false;
 }

94d345cf34e1487d7567ea8d69e3ce0c91c86194
Robert L. Walton <walton@acm.org>
2015-05-19 16:52:38 -0400

    update

---
+++ include/ll_lexeme_alternate.h
@@ -0,0 +1,154 @@
+// Layered Languages Alternate Lexical Analyzer
+//
+// File:	ll_lexeme_alternate.h
+// Author:	Bob Walton (walton@acm.org)
+// Date:	Tue May 19 15:44:38 EDT 2015
+//
+// The authors have placed this program in the public
+// domain; they make no warranty and accept no liability
+// for this program.
+
+// This file was copied from ll_lexeme_standard.h
+// on Tue May 19 15:44:38 EDT 2015.  See the earlier
+// history of that file for the earlier history of this
+// file.
+
+// Table of Contents
+//
+//	Usage and Setup
+//	Lexical Types
+//	Program Construction
+
+// Usage and Setup
+// ----- --- -----
+
+# ifndef LL_LEXEME_ALTERNATE_H
+# define LL_LEXEME_ALTERNATE_H
+
+# include <ll_lexeme.h>
+
+
+// Lexical Types
+// ------- -----
+
+namespace ll { namespace lexeme { namespace alternate {
+
+    using ll::lexeme::uns32;
+
+    // Lexeme Types
+
+    const uns32 word_t				= 1;
+    const uns32 natural_number_t		= 2;
+    const uns32 number_t			= 3;
+    const uns32 mark_t				= 4;
+    const uns32 quoted_string_t			= 5;
+    const uns32 separator_t			= 6;
+
+    const uns32 comment_t			= 7;
+    const uns32 horizontal_space_t		= 8;
+    const uns32 line_break_t			= 9;
+    const uns32 end_of_file_t			= 10;
+
+    // Erroneous Lexeme Types:
+    //
+    //    These can separated other lexemes in the same
+    //    way as horizontal space can, and if these and
+    //    horizontal space lexemes are deleted by post-
+    //    processing, they will be semantically equiva-
+    //    lent to horizontal space.
+    //
+    const uns32 premature_end_of_line_t		= 11;
+        // A line feed or <CR><LF> was encountered in a
+	// quoted string.  The quoted string is ended as
+	// if by ", and this 0-length lexeme is output
+	// before the line break.
+    const uns32 premature_end_of_file_t		= 12;
+        // A comment or quoted string ended with an end
+	// of file.  The quoted string is ended as if by
+	// ".  This 0-length lexeme is output before the
+	// end of file lexeme.
+    const uns32 misplaced_char_t		= 13;
+        // A character in a lexeme beginning position
+	// that cannot begin a lexeme.
+    const uns32 misplaced_space_t		= 14;
+        // A sequence of carriage returns, form feed,
+	// and vertical tabs that does not contain
+	// a line feed.
+
+    // Erroneous Atom Types:
+    //
+    //    These are translated to "" and ignored, so
+    //    removal will not change the lexemes produced.
+    //
+    const uns32 misplaced_control_char_t	= 15;
+        // Any control character in a quoted string.
+    const uns32 unrecognized_escape_seq_t	= 16;
+        // Any unrecognized <...> sequence in a quoted
+	// string where ... is all upper case letters
+	// and digits.
+
+    const unsigned MAX_TYPE = 16;
+
+    extern const char * const type_name[MAX_TYPE+1];
+
+    extern const char * type_code;
+        // Type codes for ll::lexeme::test_input.
+
+    namespace internal {
+
+	extern const uns8 * lexeme_type_table;
+
+    }
+
+    // Given a min::gen string that encodes an alternate
+    // lexeme, returns the type of the lexeme, with
+    // exception that number_t is returned for both
+    // natural numbers and other numbers.  Note that
+    // only words, natural numbers, numbers, marks, and
+    // separators can be encoded as strings.  In
+    // particular quoted strings, comments, horizontal
+    // space, line breaks, and end of files cannot be
+    // encoded as min::gen strings (as far as this
+    // function is concerned).
+    //
+    // Given a min:;gen value that is not a string,
+    // returns zero.  In all other cases the value
+    // returned is undefined.
+    //
+    inline uns32 lexical_type_of ( min::gen g )
+    {
+        union { uns64 b; uns8 s[8]; } v;
+	v.b = min::strhead ( g );
+	uns8 c = v.s[0];
+	if ( c == '\'' && v.s[1] != 0 )
+	    return word_t;
+	else if (     c == '.'
+	          && '0' <= v.s[1] && v.s[1] <= '9' )
+	    return number_t;
+	else
+	    return internal::lexeme_type_table[c];
+    }
+
+} } }
+
+
+// Program Construction
+// ------- ------------
+
+namespace ll { namespace lexeme { namespace alternate {
+
+    extern min::locatable_var<ll::lexeme::program>
+        default_program;
+
+    // If ll::lexeme::alternate::default_program is not
+    // NULL_STUB, this function does nothing.  Otherwise
+    // it creates the alternate lexical program in both
+    //
+    //		ll::lexeme::default_program
+    // and	ll::lexeme::alternate::default_program
+    //
+    void init_alternate_program ( void );
+
+} } }
+
+# endif // LL_LEXEME_ALTERNATE_H
---
+++ test/ll_lexeme_alternate_test.cc
@@ -0,0 +1,41 @@
+// Layered Languages Alternate Lexical Analyzer Test
+//
+// File:	ll_lexeme_alternate_test.cc
+// Author:	Bob Walton (walton@acm.org)
+// Date:	Tue May 19 15:44:38 EDT 2015
+//
+// The authors have placed this program in the public
+// domain; they make no warranty and accept no liability
+// for this program.
+//
+// This file was copied from ll_lexeme_alternate_test.cc
+// on Tue May 19 15:44:38 EDT 2015.  See the earlier
+// history of that file for the earlier history of this
+// file.
+
+# include <ll_lexeme.h>
+# include <ll_lexeme_test.h>
+# include <ll_lexeme_alternate.h>
+# include <iostream>
+# include <cassert>
+# define LEX ll::lexeme
+# define LEXSTD ll::lexeme::alternate
+
+int main ( int argc, const char * argv[] )
+{
+    min::initialize();
+    LEX::init_ostream
+	    ( LEX::default_scanner, std::cout )
+        << min::ascii;
+    LEXSTD::init_alternate_program();
+    LEX::init_program ( LEX::default_scanner,
+                        LEXSTD::default_program );
+    LEX::init_input_stream
+        ( LEX::default_scanner,
+	  std::cin,
+	    min::DISPLAY_PICTURE
+	  + min::DISPLAY_NON_GRAPHIC
+	  + min::DISPLAY_EOL );
+    LEX::test_input ( LEXSTD::type_code,
+                      LEXSTD::end_of_file_t );
+}

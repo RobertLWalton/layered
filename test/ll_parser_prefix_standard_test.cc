9264204d6763cbf00b8002e72a5238681372137a
Robert L. Walton <walton@acm.org>
2016-01-04 19:02:02 -0500

    update

--- test/ll_parser_prefix_standard_test.cc
+++ test/ll_parser_prefix_standard_test.cc
@@ -1,8 +1,8 @@
-// Layered Languages Parser Bracketed Standard Test
+// Layered Languages Parser Prefix Standard Test
 //
-// File:	ll_parser_bracketed_standard_test.cc
+// File:	ll_parser_prefix_standard_test.cc
 // Author:	Bob Walton (walton@acm.org)
-// Date:	Mon Jan  4 18:50:17 EST 2016
+// Date:	Mon Jan  4 18:50:43 EST 2016
 //
 // The authors have placed this program in the public
 // domain; they make no warranty and accept no liability
@@ -42,8 +42,6 @@ int main ( int argc, const char * argv[] )
         PAR::find_on_pass_stack
 	    ( PAR::default_parser, prefix );
     PAR::default_parser->trace_flags |=
-        bracketed_pass->trace_subexpressions
-	+
 	prefix_pass->trace_subexpressions
 	+
 	PAR::TRACE_SUBEXPRESSION_ELEMENTS
@@ -59,46 +57,5 @@ int main ( int argc, const char * argv[] )
         ( PAR::default_parser,
 	    min::DISPLAY_PICTURE );
 
-    min::locatable_gen math_name
-        ( min::new_str_gen ( "math" ) );
-    min::locatable_gen text_name
-        ( min::new_str_gen ( "text" ) );
-
-    TAB::flags math =
-        1ull << TAB::find_name
-	    ( PAR::default_parser->selector_name_table,
-	      math_name );
-    TAB::flags text =
-        1ull << TAB::find_name
-	    ( PAR::default_parser->selector_name_table,
-	      text_name );
-
-    min::locatable_gen openplus
-        ( min::new_str_gen ( "<+" ) );
-    min::locatable_gen closeplus
-        ( min::new_str_gen ( "+>" ) );
-    min::locatable_gen openminus
-        ( min::new_str_gen ( "<-" ) );
-    min::locatable_gen closeminus
-        ( min::new_str_gen ( "->" ) );
-
-    min::uns32 block_level =
-        PAR::block_level ( PAR::default_parser );
-    BRA::push_brackets
-        ( openplus,
-          closeplus,
-	  math,
-	  block_level, PAR::top_level_position,
-	  TAB::new_flags ( 0, 0, 0 ),
-	  min::NULL_STUB, min::NULL_STUB,
-	  bracketed_pass->bracket_table );
-    BRA::push_brackets
-        ( openminus,
-          closeminus,
-	  text,
-	  block_level, PAR::top_level_position,
-	  TAB::new_flags ( 0, 0, 0 ),
-	  min::NULL_STUB, min::NULL_STUB,
-	  bracketed_pass->bracket_table );
     PAR::parse();
 }

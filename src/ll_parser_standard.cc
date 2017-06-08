5f6cbdcbdafc6d521c8fa5d6eefeb04038ece500
Robert L. Walton <walton@acm.org>
2017-06-08 03:15:20 -0400

    update

--- src/ll_parser_standard.cc
+++ src/ll_parser_standard.cc
@@ -2,7 +2,7 @@
 //
 // File:	ll_parser_standard.cc
 // Author:	Bob Walton (walton@acm.org)
-// Date:	Tue Jun  6 05:29:17 EDT 2017
+// Date:	Thu Jun  8 03:15:03 EDT 2017
 //
 // The authors have placed this program in the public
 // domain; they make no warranty and accept no liability
@@ -88,6 +88,84 @@ void PARSTD::init_lexeme_map ( PAR::parser parser )
 	  data_value,
 	  PAR::PREFIX,
 	  PARLEX::data,
+	  PAR::MISSING_MASTER,
+	  parser->lexeme_map );
+
+    min::locatable_gen raw_data
+        ( min::new_lab_gen ( "raw", "data" ) );
+    min::gen raw_data_t_name =
+        LEX::lexeme_type_name
+	    ( LEXSTD::raw_data_t, parser->scanner );
+    min::locatable_gen raw_data_value
+        ( min::new_obj_gen ( 10, 1 ) );
+    {
+        min::obj_vec_insptr vp ( raw_data_value );
+	min::attr_insptr ap ( vp );
+	min::locate ( ap, min::dot_type );
+	min::set ( ap, raw_data );
+    }
+
+    TAB::push_lexeme_map_entry
+        ( raw_data_t_name,
+	  LEXSTD::raw_data_t,
+	  PAR::TOP_LEVEL_SELECTOR,
+	  block_level,
+	  PAR::top_level_position,
+	  raw_data_value,
+	  PAR::PREFIX,
+	  raw_data,
+	  PAR::MISSING_MASTER,
+	  parser->lexeme_map );
+
+    min::locatable_gen table
+        ( min::new_str_gen ( "table" ) );
+    min::gen table_t_name =
+        LEX::lexeme_type_name
+	    ( LEXSTD::table_t, parser->scanner );
+    min::locatable_gen table_value
+        ( min::new_obj_gen ( 10, 1 ) );
+    {
+        min::obj_vec_insptr vp ( table_value );
+	min::attr_insptr ap ( vp );
+	min::locate ( ap, min::dot_type );
+	min::set ( ap, table );
+    }
+
+    TAB::push_lexeme_map_entry
+        ( table_t_name,
+	  LEXSTD::table_t,
+	  PAR::TOP_LEVEL_SELECTOR,
+	  block_level,
+	  PAR::top_level_position,
+	  table_value,
+	  PAR::PREFIX,
+	  table,
+	  PAR::MISSING_MASTER,
+	  parser->lexeme_map );
+
+    min::locatable_gen row
+        ( min::new_str_gen ( "row" ) );
+    min::gen row_t_name =
+        LEX::lexeme_type_name
+	    ( LEXSTD::row_t, parser->scanner );
+    min::locatable_gen row_value
+        ( min::new_obj_gen ( 10, 1 ) );
+    {
+        min::obj_vec_insptr vp ( row_value );
+	min::attr_insptr ap ( vp );
+	min::locate ( ap, min::dot_type );
+	min::set ( ap, row );
+    }
+
+    TAB::push_lexeme_map_entry
+        ( row_t_name,
+	  LEXSTD::row_t,
+	  PAR::TOP_LEVEL_SELECTOR,
+	  block_level,
+	  PAR::top_level_position,
+	  row_value,
+	  PAR::PREFIX,
+	  row,
 	  PAR::MISSING_MASTER,
 	  parser->lexeme_map );
 }

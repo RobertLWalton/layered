fe913afc2e11d1a2bf63726ec55059222db390a2
Robert L Walton <walton@acm.org>
2021-02-14 05:17:46 -0500

    update

--- src/ll_parser_standard_oper.cc
+++ src/ll_parser_standard_oper.cc
@@ -2,7 +2,7 @@
 //
 // File:	ll_parser_standard_oper.cc
 // Author:	Bob Walton (walton@acm.org)
-// Date:	Sat Dec 12 01:50:53 EST 2020
+// Date:	Sun Feb 14 05:16:35 EST 2021
 //
 // The authors have placed this program in the public
 // domain; they make no warranty and accept no liability
@@ -41,8 +41,6 @@ OP::oper_pass PARSTD::init_oper
         ( min::new_str_gen ( "code" ) );
     min::locatable_gen math_name
         ( min::new_str_gen ( "math" ) );
-    min::locatable_gen data_name
-        ( min::new_str_gen ( "data" ) );
 
     TAB::flags code =
         1ull << TAB::find_name
@@ -50,11 +48,8 @@ OP::oper_pass PARSTD::init_oper
     TAB::flags math =
         1ull << TAB::find_name
 	    ( parser->selector_name_table, math_name );
-    TAB::flags data =
-        1ull << TAB::find_name
-	    ( parser->selector_name_table, data_name );
 
-    oper_pass->selectors = code | math | data;
+    oper_pass->selectors = code | math;
 
     min::locatable_gen declare
         ( min::new_str_gen ( "declare" ) );

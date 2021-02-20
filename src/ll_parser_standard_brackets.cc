2a1d7a235bf29ba7e8cb5b4adea6f88669651456
Robert L Walton <walton@acm.org>
2021-02-20 06:36:08 -0500

    update

--- src/ll_parser_standard_brackets.cc
+++ src/ll_parser_standard_brackets.cc
@@ -2,7 +2,7 @@
 //
 // File:	ll_parser_standard_brackets.cc
 // Author:	Bob Walton (walton@acm.org)
-// Date:	Sat Feb 20 01:13:18 EST 2021
+// Date:	Sat Feb 20 06:23:37 EST 2021
 //
 // The authors have placed this program in the public
 // domain; they make no warranty and accept no liability
@@ -110,11 +110,6 @@ void PARSTD::init_brackets ( PAR::parser parser )
     min::locatable_gen angle_closing_square
         ( min::new_lab_gen ( ">", "]" ) );
 
-    min::locatable_gen opening_brace_dollar
-        ( min::new_lab_gen ( "{", "$" ) );
-    min::locatable_gen dollar_closing_brace
-        ( min::new_lab_gen ( "$", "}" ) );
-
     min::locatable_gen opening_square_dollar
         ( min::new_lab_gen ( "[", "$" ) );
     min::locatable_gen dollar_closing_square
@@ -188,7 +183,6 @@ void PARSTD::init_brackets ( PAR::parser parser )
 	    ( min::gen_packed_vec_type.new_stub ( 6 ) );
     min::push ( text_arguments ) = s;
     min::push ( text_arguments ) = period;
-    min::push ( text_arguments ) = period;
     min::push ( text_arguments ) = question;
     min::push ( text_arguments ) = exclamation;
     min::push ( text_arguments ) = PARLEX::colon;
@@ -228,14 +222,6 @@ void PARSTD::init_brackets ( PAR::parser parser )
 	      ( special_name,
 	        BRA::untyped_reformatter_stack ),
 	  min::NULL_STUB,
-	  bracketed_pass->bracket_table );
-    BRA::push_brackets
-        ( opening_brace_dollar,
-          dollar_closing_brace,
-	  code,
-	  block_level, PAR::top_level_position,
-	  TAB::new_flags ( 0, 0, 0 ),
-	  min::NULL_STUB, min::NULL_STUB,
 	  bracketed_pass->bracket_table );
 
     BRA::push_indentation_mark
